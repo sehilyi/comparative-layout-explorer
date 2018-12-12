2fd053c8ec8b8faf5166e71da14ae89416f864ed
sehilyi <shlyi@hcil.snu.ac.kr>
2018-12-12 17:43:13 +0900

    Temp fix for msssim calculation

--- src/metrics/index.ts
+++ src/metrics/index.ts
@@ -14,9 +14,9 @@ export function getSSIM(a: any[], b: any[]) {
 export function getMSSSIM(a: any[], b: any[]) {
   let imgData1: ImageSSIM.IImage = {data: a, width: CHART_TOTAL_SIZE.width, height: CHART_TOTAL_SIZE.height, channels: 4};
   let imgData2: ImageSSIM.IImage = {data: b, width: CHART_TOTAL_SIZE.width, height: CHART_TOTAL_SIZE.height, channels: 4};
-  let imagePair = [imgData1, imgData2, 8, false];
+  let imagePair = [imgData1, imgData2, 9, false];
   let res = ImageMSSSIM.compare(imagePair[0] as ImageSSIM.IImage, imagePair[1] as ImageSSIM.IImage, imagePair[2] as number, 0.01, 0.03, imagePair[3] as boolean);
-  return res.msssim;  //TODO: giving NaN
+  return res.msssim;
 }
 
 export function getMSE(a: any[], b: any[]) {
