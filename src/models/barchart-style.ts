61c62ee2a992d0585aaafc6215d33abd4fb6b3ca
Sehi L'Yi <shlyi@hcil.snu.ac.kr>
2019-01-25 09:38:05 +0900

    Add bar style

---
+++ src/models/barchart-style.ts
@@ -0,0 +1,15 @@
+export type barStyle = {
+  noX: boolean
+  noY: boolean
+  revX: boolean
+  revY: boolean
+  noGrid: boolean
+  xName: string
+  barGap: number
+  width: number
+  height: number
+  altVals: object[]
+  stroke: number
+  stroke_width: number
+  legend: number[]
+}
